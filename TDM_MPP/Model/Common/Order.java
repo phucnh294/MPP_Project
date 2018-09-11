56c14025e77ffc5b00e700d0d5f2f7929b2d409e
Phuc Nguyen <phucnh294@gmail.com>
2018-09-11 15:23:20 -0500

    add

--- TDM_MPP/Model/Common/Order.java
+++ TDM_MPP/Model/Common/Order.java
@@ -1,5 +1,27 @@
 package Common;
 
-public class Order {
+import java.time.LocalDate;
 
+public class Order {
+	private int id;
+	private int amount;
+	private LocalDate orderDate;
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
