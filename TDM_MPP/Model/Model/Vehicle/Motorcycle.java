dbced3f761848fe6c0c105e13d320aa86ef766e6
986338 <986338@cslab-m105-105.cs.mum.edu>
2018-09-11 14:13:38 -0500

    sample data

--- TDM_MPP/Model/Model/Vehicle/Motorcycle.java
+++ TDM_MPP/Model/Model/Vehicle/Motorcycle.java
@@ -1,5 +1,25 @@
 package Model.Vehicle;
 
 public class Motorcycle {
-
+	private int fuelType;
+	private int numberOfEngine;
+	private int tranmissionType;
+	public int getTranmissionType() {
+		return tranmissionType;
+	}
+	public void setTranmissionType(int tranmissionType) {
+		this.tranmissionType = tranmissionType;
+	}
+	public int getNumberOfEngine() {
+		return numberOfEngine;
+	}
+	public void setNumberOfEngine(int numberOfEngine) {
+		this.numberOfEngine = numberOfEngine;
+	}
+	public int getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(int fuelType) {
+		this.fuelType = fuelType;
+	}
 }
