e06e3c064ea0bf8dbb1bfa33b5a8ee9dd9f915cb
Phuc Nguyen <phucnh294@gmail.com>
2018-09-11 14:23:38 -0500

    add user

--- TDM_MPP/Model/Model/User/User.java
+++ TDM_MPP/Model/Model/User/User.java
@@ -1,5 +1,54 @@
 package Model.User;
 
-public class User {
-
+import java.time.LocalDate;
+ public class User {
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private int phone;
+	private String address;
+	private LocalDate birthDate;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
