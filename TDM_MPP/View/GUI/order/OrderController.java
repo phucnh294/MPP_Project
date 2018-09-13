b86a6b3fe3d56efcb80d03bb406fb99741a323f9
Phuc Nguyen <phucnh294@gmail.com>
2018-09-12 22:40:45 -0500

    update customer

--- TDM_MPP/View/GUI/order/OrderController.java
+++ TDM_MPP/View/GUI/order/OrderController.java
@@ -3,6 +3,8 @@ package GUI.order;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class OrderController implements Initializable{
@@ -14,5 +16,9 @@ public class OrderController implements Initializable{
 	}
 	
 	
-
+	@FXML protected void onCreateOrder_Click(ActionEvent event) {}	
+	@FXML protected void onSearchOrder_Click(ActionEvent event) {}
+	@FXML protected void onClearOrder_Click(ActionEvent event) {}
+	@FXML protected void onRemoveOrder_Click(ActionEvent event) {}
+	@FXML protected void onEditOrder_Click(ActionEvent event) {}
 }
