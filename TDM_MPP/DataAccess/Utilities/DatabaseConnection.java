8d9dadd50b0ef4f004f4b016360c83e2d697e7d3
Kien Pham <ptkien@ymail.com>
2018-09-12 22:04:43 -0500

    add setting file

--- TDM_MPP/DataAccess/Utilities/DatabaseConnection.java
+++ TDM_MPP/DataAccess/Utilities/DatabaseConnection.java
@@ -3,6 +3,9 @@
  */
 package Utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 /**
  * @author ptkie
  *
@@ -10,22 +13,45 @@ package Utilities;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
 	private static DatabaseConnection instance;
 	private Connection connection;
-	private String url = "jdbc:mysql://localhost:3306/TDM?useSSL=false";
+	private String url = "jdbc:mysql://localhost:3306/TDM";
 	private String username = "root";
 	private String password = "root";
 
 	private DatabaseConnection() throws SQLException {
+		readSettings();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			this.connection = DriverManager.getConnection(url, username, password);
 		} catch (ClassNotFoundException ex) {
 			System.out.println("Database Connection Creation Failed : " + ex.getMessage());
 		}
+	}
+
+	private void readSettings() {
+		Properties props = new Properties();
+		FileInputStream in;
+		try {
+			in = new FileInputStream("setting.properties");
+			props.load(in);
+			in.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("setting.properties is not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Can not read setting.properties file");
+			e.printStackTrace();
+		}
+		url = props.getProperty("jdbc.url", "jdbc:mysql://localhost:3306/TDM");
+		username = props.getProperty("jdbc.username", "root");
+		password = props.getProperty("jdbc.password", "root");
+
 	}
 
 	public Connection getConnection() {
@@ -35,8 +61,12 @@ public class DatabaseConnection {
 	public static DatabaseConnection getInstance() throws SQLException {
 		if (instance == null || instance.getConnection().isClosed()) {
 			instance = new DatabaseConnection();
-		} 
+		}
 
 		return instance;
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		DatabaseConnection.getInstance();
 	}
 }
