d3f220c4f5369003f839dd6257d7e4b410f85180
Kien Pham <ptkien@ymail.com>
2018-09-13 22:32:30 -0500

    format code

--- TDM_MPP/Common/Data/Party.java
+++ TDM_MPP/Common/Data/Party.java
@@ -1,10 +1,67 @@
 package Data;
 
-public class Party extends User {
+import java.time.LocalDate;
+
+public class Party extends Entity {
+	private String username;
+	private String password;
+
+	private String email;
+	private String phone;
+	private String address;
+	private LocalDate birthDate;
 	
 	private String type;
 	private String hobbies;
 	private int marriageStatus;	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	public int getMarriageStatus() {
 		return marriageStatus;
